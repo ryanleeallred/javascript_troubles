4e2b9330484c3b849776b4db143f05f9ee7a2264
Ryan Allred <ryanleeallred@gmail.com>
2018-09-28 13:07:55 -0600

    removes double curlies

--- pandas_bootstrap_table.js
+++ pandas_bootstrap_table.js
@@ -9,7 +9,7 @@ $(function() {
     paginationVAlign: 'both',
     pageSize: 25,
     pageList: [10, 25, 50, 100, 'ALL'],
-    columns: {{ columns|safe }},  // here is where we use the column content from our Django View
-    data: {{ data|safe }}, // here is where we use the data content from our Django View. we escape the content with the safe tag so the raw JSON isn't shown.
+    columns: { columns|safe },  // here is where we use the column content from our Django View
+    data: { data|safe }, // here is where we use the data content from our Django View. we escape the content with the safe tag so the raw JSON isn't shown.
   });
 });
